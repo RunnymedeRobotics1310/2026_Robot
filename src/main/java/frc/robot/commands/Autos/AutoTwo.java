4a3ab452d5cbaaf56ee54ad74efb555529158598
carson-JH <carson.honeyman@gmail.com>
2026-02-11 16:28:31 -0500

    Fixed wait

--- src/main/java/frc/robot/commands/Autos/AutoTwo.java
+++ src/main/java/frc/robot/commands/Autos/AutoTwo.java
@@ -1,18 +1,14 @@
 package frc.robot.commands.Autos;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.swerve.NullDriveCommand;
 import frc.robot.subsystems.swerve.SwerveSubsystem;
 
 public class AutoTwo extends SequentialCommandGroup {
 
   public AutoTwo(SwerveSubsystem swerve, double delay) {
-    addCommands(new WaitCommand(delay));
+    addCommands(new NullDriveCommand(swerve).withTimeout(delay));
 
-    addCommands(
-        new TestAuto(swerve, delay)
-            .withTimeout(0.5)
-            .andThen(new WaitCommand(1))
-            .andThen(new TestAuto(swerve, delay)));
+    addCommands(new TestAuto(swerve, delay).withTimeout(0.5).andThen(new TestAuto(swerve, delay)));
   }
 }
