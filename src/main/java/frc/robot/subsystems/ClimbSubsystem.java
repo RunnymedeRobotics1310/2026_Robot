6c399b05b02b7334dd70ef96f1516dc447e9bf74
q-field <q@quentinfield.com>
2026-02-27 17:16:49 -0500

    update ClimbSubsystem

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -6,6 +6,8 @@ package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.SparkMax;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import static frc.robot.Constants.ClimbConstants.*;
@@ -15,12 +17,9 @@ public class ClimbSubsystem extends SubsystemBase {
     private final SparkMax primaryClimbMotor = new SparkMax(50, SparkLowLevel.MotorType.kBrushless);
     private final SparkMax secondaryClimbMotor = new SparkMax(51, SparkLowLevel.MotorType.kBrushless);
 
-    private final SparkMax leftHookMotor = new SparkMax(52, SparkLowLevel.MotorType.kBrushless);
-    private final SparkMax rightHookMotor = new SparkMax(53, SparkLowLevel.MotorType.kBrushless);
+    private final Solenoid hookSolenoid = new Solenoid(PneumaticsModuleType.CTREPCM, 1);
 
     private double climbMotorSpeed = 0;
-    private double leftHookSetpoint = 0;
-    private double rightHookSetpoint = 0;
 
 
   public ClimbSubsystem() {
@@ -45,51 +44,14 @@ public class ClimbSubsystem extends SubsystemBase {
     primaryClimbMotor.set(climbMotorSpeed);
     secondaryClimbMotor.set(climbMotorSpeed);
 
-      // left hook
-      double leftDelta = leftHookSetpoint - getLeftHookEncoder();
-      if (Math.abs(leftDelta) <= HOOK_TOLERANCE) {
-          leftHookMotor.set(0);
-      } else if (leftDelta > 0) leftHookMotor.set(-0.1);
-      else leftHookMotor.set(0.1);
-
-      // right hook
-      double rightDelta = rightHookSetpoint - getRightHookEncoder();
-      if (Math.abs(rightDelta) <= HOOK_TOLERANCE) {
-          rightHookMotor.set(0);
-      } else if (leftDelta > 0) rightHookMotor.set(-0.1);
-      else rightHookMotor.set(0.1);
-
   }
 
     public void setClimbSpeed(double speed) {
         climbMotorSpeed = speed;
     }
 
-    public void setLeftHookPosition(double position) {
-        leftHookSetpoint = position;
+    public void deployHook() {
+        hookSolenoid.set(true);
     }
-    public double getLeftHookEncoder() {
-      return leftHookMotor.getEncoder().getPosition();
-    }
-    public void setRightHookPosition(double position) {
-        rightHookSetpoint = position;
-    }
-    public double getRightHookEncoder() {
-        return rightHookMotor.getEncoder().getPosition();
-    }
-
-    public void extendLeftHook(){
-      setLeftHookPosition(HOOK_EXTENDED_POSITION);
-    }
-    public void retractLeftHook(){
-      setLeftHookPosition(0);
-    }
-    public void extendRightHook(){
-      setRightHookPosition(HOOK_EXTENDED_POSITION);
-    }
-    public void retractRightHook(){
-      setRightHookPosition(0);
-    }
-    
 
 }
