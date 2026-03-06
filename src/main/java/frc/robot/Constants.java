02883d1ea04e8098a99e13dded1fb8bde46f59bb
q-field <q@quentinfield.com>
2026-03-06 17:32:14 -0500

    comment

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -103,7 +103,7 @@ public final class Constants {
     public static final double MAX_SHOOTER_RPM = 6000;
     public static final double KP = 0.0003;
     public static final double KI = 0;
-    public static final double KD = 0;
+    public static final double KD = 0; // keep this at zero
     public static final double KFF = 1 / MAX_SHOOTER_RPM;
     public static final double KICKER_RUNSPEED = 1;
     public static final double AGITATOR_RUNSPEED = -0.6;
