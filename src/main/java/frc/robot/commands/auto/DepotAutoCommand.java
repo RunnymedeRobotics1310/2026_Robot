a15c5d91daef86082bfbb196718e4146a8fae50c
carson-JH <carson.honeyman@gmail.com>
2026-04-15 16:56:40 -0400

    Very good depot auto changes

--- src/main/java/frc/robot/commands/auto/DepotAutoCommand.java
+++ src/main/java/frc/robot/commands/auto/DepotAutoCommand.java
@@ -23,31 +23,35 @@ public class DepotAutoCommand extends SequentialCommandGroup {
 
     addCommands(new NullDriveCommand(swerve).withTimeout(delay));
 
-    addCommands(
-        new DriveToFieldLocationAimedAtHubCommand(
-                swerve, new Pose2d(1.1, 6, new Rotation2d()), 0.3, 0.3)
-            .deadlineFor(new AutoShooterCommand(hopper, swerve, 8)));
-
     //    addCommands(
     //        new DriveToFieldLocationAimedAtHubCommand(
-    //                swerve, new Pose2d(0.89, 5.2, new Rotation2d()), 0.3, 0.3)
+    //                swerve, new Pose2d(1.1, 6, new Rotation2d()), 0.3, 0.3)
     //            .deadlineFor(new AutoShooterCommand(hopper, swerve, 8)));
 
-    //    addCommands(new FaceAngleCommand(swerve, Rotation2d.fromDegrees(60)));
-
-    addCommands(new FaceAngleCommand(swerve, Rotation2d.fromDegrees(90)));
-
     addCommands(
-        new DriveFieldOrientedCommand(swerve, -0.4, 0, 90)
-            .withTimeout(3)
-            .deadlineFor(new LazyIntakeCommand(hopper)));
-    addCommands(new DriveFieldOrientedCommand(swerve, 1, 0, 0).withTimeout(0.8));
+        (new DriveToFieldLocationAimedAtHubCommand(
+                    swerve, new Pose2d(1.5, 5.2, new Rotation2d()), 0.3, 0.3)
+                .deadlineFor(new AutoShooterCommand(hopper, swerve, 8)))
+            .andThen(
+                (new DriveToFieldLocationCommand(
+                            swerve, new Pose2d(0.89, 5.2, Rotation2d.fromDegrees(50)), 0.3)
+                        .andThen(
+                            new DriveFieldOrientedCommand(swerve, -0.1, 0.8, 50).withTimeout(2.3)))
+                    .deadlineFor(new LazyIntakeCommand(hopper))));
+
+    //    addCommands(
+    //        new DriveFieldOrientedCommand(swerve, -0.4, 0, 90)
+    //            .withTimeout(3)
+    //            .deadlineFor(new LazyIntakeCommand(hopper)));
+    //    addCommands(new DriveFieldOrientedCommand(swerve, 1, 0, 0).withTimeout(0.8));
 
     //    addCommands(
     //        (new DriveToFieldLocationCommand(swerve, new Pose2d(0.55, 5.1, new Rotation2d(60)),
     // 0.3)
     //                .andThen(new DriveFieldOrientedCommand(swerve, 0, 1, 54).withTimeout(2)))
     //            .deadlineFor(new LazyIntakeCommand(hopper)));
+
+    addCommands(new DriveFieldOrientedCommand(swerve, 1, 0, 0).withTimeout(1));
 
     addCommands(
         ((new AutoShooterCommand(hopper, swerve, 1310))
